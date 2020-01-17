d013f0b50c0f56300c3b9a9b09919825c352dd53
Mario90 <mgpinan1990@gmail.com>
2020-01-17 21:24:53 +0000

    Collaz conjecture

---
+++ collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,21 @@
+//
+// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const steps = (n) => {
+  if (n == 0 || n < 0) throw new Error('Only positive numbers are allowed')
+  if (n >= 1) {
+    let count = 0
+    while (n !== 1) {
+    if (n % 2 === 0) {
+        n = n / 2
+        count++
+      } else {
+          n = (n * 3) + 1  
+          count++
+        }
+      }
+      return count
+    }
+};
