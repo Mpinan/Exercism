e5871c7b1c7cd24eddc4adbb567bc1464e051944
Mario90 <mgpinan1990@gmail.com>
2020-01-20 13:27:48 +0000

    Added letters

--- roman-numerals/roman-numerals.js
+++ roman-numerals/roman-numerals.js
@@ -4,6 +4,21 @@
 //
 
 export const toRoman = n => {
+  let Letters = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XV: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
   if (n === 1) {
     return "I";
   } else {
