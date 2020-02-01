efdc6906d6ba83225026f8be38ab110fd6c8c217
Mario90 <mgpinan1990@gmail.com>
2020-02-01 17:31:53 +0000

    Gigasecond solved

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -5,6 +5,8 @@
 
 export const gigasecond = date => {
   let ms = Math.pow(10, 12);
+
+  //10^9 will give 1.000.000.000 which is represented in seconds.
   //getTime return miliseconds that's why Math.pow is to 12 and not to 9
 
   let time = date.getTime();
